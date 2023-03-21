6cbbd5fb6d56e44690cad69242987dc542235c03
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-03-21 19:52:55 +0000

    Update mapFun.py

--- mapFun.py
+++ mapFun.py
@@ -6,7 +6,11 @@ def mapFunc(fun,x):
     if isinstance(x,set):return set(map(lambda n:mapFunc(fun,n),x))
     if isinstance(x,frozenset):return frozenset(map(lambda n:mapFunc(fun,n),x))
     if isinstance(x,dict):return dict(map(lambda n:(n[0],mapFunc(fun,n[1])),x.items()))
-
+    return 0
+def mapfunc(fun,*y):
+    z=list(map(lambda n:mapFunc(fun,n),y))
+    if len(z)<2:return z[0]
+    return z
 print(mapFunc(lambda a : a + 1,4))
 print(mapFunc(lambda a : a + 1,4.2))
 print(mapFunc(lambda a : len(a),"Ziko"))
@@ -16,3 +20,6 @@ print(mapFunc(lambda a : a + 5,(1, 2, 3, [4])))
 print(mapFunc(lambda a : a + 5,{1,2,3}))
 print(mapFunc(lambda a : a + 5,frozenset({1,2,3})))
 print(mapFunc(lambda a : a + 5,{"a":1,"b":2,"c":3}))
+
+print(mapfunc(lambda x:x+1,1,2,3,[1,2]))
+print(mapfunc(lambda x:x+1,1,2,3,[1,2]))
