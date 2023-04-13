a33465eb2a7070861631d892875af42177d02799
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-13 15:11:38 +0000

    Update mapFun.js

--- Javascript/mapFun.js
+++ Javascript/mapFun.js
@@ -6,7 +6,8 @@ const mapFun=(fun,...X)=>{
         if(ArrayBuffer.isView(x))return x.map(n=>fun(n));
         if(x instanceof Set)return new Set(mapFun(fun,...[...x]));
         if(x instanceof Map)return new Map([...x].map(n=>[n[0],mapFun(fun,n[1])]));
-        if(x instanceof Object)return Object.fromEntries(Object.entries(x).map(n=>n=[n[0],mapFun(fun,n[1])]))
+        if(x instanceof Object)return Object.fromEntries(Object.entries(x).map(n=>n=[n[0],mapFun(fun,n[1])]));
+        else throw Error("Uncategorised data")
     });
    return Y.length==1?Y[0]:Y; 
 }
