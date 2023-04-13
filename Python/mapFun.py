8472b620b92d313c67cbdc313a7b5cf158a15ff0
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-13 13:28:51 +0000

    Update mapFun.py

--- Python/mapFun.py
+++ Python/mapFun.py
@@ -11,15 +11,3 @@ def mapfun(fun,*y):
     z=list(map(lambda n:mapFun(fun,n),y))
     if len(z)<2:return z[0]
     return z
-print(mapFun(lambda a : a + 1,4))
-print(mapFun(lambda a : a + 1,4.2))
-print(mapFun(lambda a : len(a),"Ziko"))
-print(mapFun(lambda a : a + 5,[1, 2, 3, [4]]))
-print(mapFun(lambda a : a + 5,range(0,10)))
-print(mapFun(lambda a : a + 5,(1, 2, 3, [4])))
-print(mapFun(lambda a : a + 5,{1,2,3}))
-print(mapFun(lambda a : a + 5,frozenset({1,2,3})))
-print(mapFun(lambda a : a + 5,{"a":1,"b":2,"c":3}))
-
-print(mapfun(lambda x:x+1,1,2,3,[1,2]))
-print(mapfun(lambda x:x+1,1,2,3,[1,2]))
