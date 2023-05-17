56e356fa052a11e7ab04869b5c1dde081dc991ca
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-17 19:49:28 +0100

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -7,7 +7,10 @@ const mapFun = (fun, { skip = [], key = false, value = true } = {}, ...X) => {
     if (ArrayBuffer.isView(x)) return Array.from(x).map((n) => fun(n));
     if (x instanceof Set) return new Set(mapFun(fun, ...[...x]));
     if (x instanceof Map) return new Map([...x].map((n) => [n[0], mapFun(fun, n[1])]));
-    if (x instanceof Object) return Object.fromEntries(Object.entries(x).map((n) => (n = [n[0], mapFun(fun, n[1])])));
+    if (x instanceof Object) return Object.fromEntries(
+      Object.entries(x).map(([key, value]) => [key, mapFun(fun, value)])
+    )
+    
     else throw new Error('Uncategorised data');
   });
     return Y.length === 1 ? Y[0] : Y;
