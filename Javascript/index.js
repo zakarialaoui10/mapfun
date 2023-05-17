f75f505623ae9369a6e8135deee777fb4754cc47
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-17 19:30:00 +0100

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -12,14 +12,8 @@ const mapFun = (fun, { skip = [], key = false, value = true } = {}, ...X) => {
     if (x instanceof Object) return Object.fromEntries(Object.entries(x).map((n) => (n = [n[0], mapFun(fun, n[1])])));
     else throw new Error('Uncategorised data');
   });
-
-  if (isNodeModule) {
     return Y.length === 1 ? Y[0] : Y;
-  } else {
-    window.mapFun = mapFun;
-  }
 };
-
 if (typeof module !== 'undefined' && typeof exports !== 'undefined') {
   module.exports = { mapFun };
 }
