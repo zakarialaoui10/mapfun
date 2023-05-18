512493548f96fb5c02eef7889414e1a9a6a324c2
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-18 13:07:13 +0100

    Update mapFun.py

--- Python/mapFun.py
+++ Python/mapFun.py
@@ -1,3 +1,12 @@
+def split_skip(skip):
+    skipPrimitives = []
+    skipObjects = []
+    for element in skip:
+        if isinstance(element, object) and element is not None:
+            skipObjects.append(element)
+        else:
+            skipPrimitives.append(element)
+    return list([skipPrimitives,skipObjects])
 def mapFun(fun,x):
     if isinstance(x,(int,float,complex,str,bool)):return fun(x)
     if isinstance(x,list):return list(map(lambda n:mapFun(fun,n),x))
