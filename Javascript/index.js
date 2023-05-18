c46f6fee1213ef7e6faedd0c3b820c339127ad58
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-18 12:00:45 +0100

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,10 +3,15 @@ const mapFun = (fun, { skip = [], key = false, value = true } = {}, ...X) => {
     if (typeof skip === 'string' && typeof x === skip) return x;
     if (x === null) return fun(null);
     if (['number', 'string', 'boolean', 'bigint', 'undefined'].includes(typeof x)) return fun(x);
-    if (x instanceof Array) return x.map((n) => mapFun(fun, n));
+    if (x instanceof Array) return x.map((n) => mapFun(fun,{},n));
     if (ArrayBuffer.isView(x)) return Array.from(x).map((n) => fun(n));
-    if (x instanceof Set) return new Set(mapFun(fun, ...[...x]));
-    if (x instanceof Map) return new Map([...x].map((n) => [n[0], mapFun(fun, n[1])]));
+    if (x instanceof Set) return new Set(mapFun(fun,{},...[...x]));
+    if (x instanceof Map) return new Map([...x].map(n =>{
+        return [
+            key?mapFun(fun,{},n[0]):n[0],
+            value?mapFun(fun,{},n[1]):n[1],
+            ]
+    }));
     if (x instanceof Object) return Object.fromEntries(
       Object.entries(x).map(([KEY, VALUE]) => [
         key?mapFun(fun,{},KEY):KEY,
@@ -21,3 +26,5 @@ const mapFun = (fun, { skip = [], key = false, value = true } = {}, ...X) => {
 if (typeof module !== 'undefined' && typeof exports !== 'undefined') {
   module.exports = { mapFun };
 }
+a=new Map([["a",1],["b",2]]);
+console.log(mapFun(n=>n+1,{},a))
