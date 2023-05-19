63f3e8113f991139d9eff9e1aab644872ec7c9c9
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-19 11:10:28 +0100

    Update and rename mapFun.php to mapfunc.php

--- Php/mapfunc.php
+++ Php/mapfunc.php
@@ -1,5 +1,5 @@
 <?php
-function mapFun($fun, $options = [], ...$X) {
+function mapfunc($fun, $options = [], ...$X) {
   $skip = isset($options['skip']) ? $options['skip'] : [];
   $key = isset($options['key']) ? $options['key'] : false;
   $value = isset($options['value']) ? $options['value'] : true; 
@@ -13,14 +13,14 @@ function mapFun($fun, $options = [], ...$X) {
     if (in_array(gettype($x), ['integer', 'double', 'boolean', 'string']))return $fun($x);
     if (is_array($x)) {
       return array_map(function($n) use ($fun) {
-        return mapFun($fun, [], $n);
+        return mapfunc($fun, [], $n);
       }, $x);
     }
     if (is_object($x)) {
       return array_reduce(array_map(function($KEY, $VALUE) use ($fun, $key, $value) {
         return [
           $KEY,
-          $value ? mapFun($fun, [], $VALUE) : $VALUE,
+          $value ? mapfunc($fun, [], $VALUE) : $VALUE,
         ];
       }, array_keys(get_object_vars($x)), array_values(get_object_vars($x))), function($carry, $item) {
         [$KEY, $VALUE] = $item;
