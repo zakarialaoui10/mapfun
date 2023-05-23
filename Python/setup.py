8b2a4bb3635a33bf0d92daf922aedf5c7e1c22e9
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-23 16:29:54 +0100

    Update setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -1,1 +1,27 @@
+from setuptools import setup
 
+VERSION = '0.0.1'
+DESCRIPTION = 'mapfun'
+LONG_DESCRIPTION = 'A function that applies a mapping function to an infinite number of input elements, with options to skip certain elements and selectively apply the mapping to keys and/or values of objects.'
+
+# Setting up
+setup(
+    name="mapfun",
+    version=VERSION,
+    author="Zakaria Elalaoui",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages = ['mapfun'],
+    install_requires=[],
+    keywords=['python', 'map'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
