5939c5deef32bf9ef51309a6c8d921ad2e590120
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-05-24 17:02:24 +0100

    Update setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 from pathlib import Path
-VERSION = '0.0.3'
-parent = Path(__file__).parent
+VERSION = '0.0.5'
 setup(
     name="mapfun",
     version=VERSION,
     author="Zakaria Elalaoui",
     description="mapfun",
     long_description_content_type="text/markdown",
-    long_description=(parent / "README.md").read_text(),
-    packages = ['src'],
+    long_description=(Path(__file__).resolve().parent / "README.md").read_text(encoding='utf-8'),
+    packages = ['mapfun'],
     keywords=['zikojs','python', 'map'],
     classifiers=[
         "Development Status :: 1 - Planning",
