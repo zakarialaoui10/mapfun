67e5d4914e305f5e9aca3ab6735e20900d1efc74
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-07-11 11:49:25 +0100

    fix go

--- go/index.go
+++ go/index.go
@@ -48,20 +48,20 @@ func mapfunRecursive(callback func(float64) float64, value any) any {
 	}
 }
 
-// func main() {
-// 	// Use math.Cos as the callback function
-// 	callback := math.Cos
+func main() {
+	// Use math.Cos as the callback function
+	callback := math.Cos
 
-// 	// Define separate structures to be processed by mapfun
-// 	a := []any{1, 2, 3, []any{4, 5}}
-// 	b := map[string]any{
-// 		"a": 6,
-// 		"b": []any{7, 8, map[string]float64{"c": 9}},
-// 	}
-// 	c := 10.0
+	// Define separate structures to be processed by mapfun
+	a := []any{1, 2, 3, []any{4, 5}}
+	b := map[string]any{
+		"a": 6,
+		"b": []any{7, 8, map[string]float64{"c": 9}},
+	}
+	c := 10.0
 
-// 	// Apply mapfun with multiple arguments
-// 	result := mapfun(callback, a, b, c)
+	// Apply mapfun with multiple arguments
+	result := mapfun(callback, a, b, c)
 
-// 	fmt.Println(result)
-// }
+	fmt.Println(result)
+}
